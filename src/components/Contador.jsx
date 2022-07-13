66bfa1fa77c502f070e6b3aeb2f60f88407d1634
João Albuquerque <joao.albuquerque@quintoandar.com.br>
2022-07-12 22:14:51 -0300

    Introdução ao React com Hooks

---
+++ src/components/Contador.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Contador() {
+  const [contador, setContador] = useState(500);
+
+  useEffect(() => {
+    // O que será executado quando o evento for iniciado
+    console.log("Componente iniciado");
+    const intervalo = setInterval(() => {
+      setContador((valor) => valor - 1);
+    }, 1000);
+    // Contar de um em um segundo (1000ms)
+
+    return () => {
+      // O que será executado quando o evento de destruição
+      // do componente for iniciado
+      console.log("Componente destruído");
+      // Desinscrição de evento
+      clearInterval(intervalo);
+    };
+    // O array de condições para os eventos acima
+  }, []);
+
+  // O array vazio indica que o useEffect
+  // executará apenas uma vez na inicialização do componente
+
+  return <div>Contador: {contador} </div>;
+}
---
+++ src/components/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from "react";
+
+const Todo = () => {
+  // Estado
+  const [todos, setTodos] = useState([]);
+
+  // Data Binding
+  // const input = document.querySelector("[name='todo']")
+  const inputText = useRef();
+
+  // Ciclo de Vida
+  useEffect(() => {
+    console.log("Componente Construído!");
+    const existingTodos = localStorage.getItem("todos");
+    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
+    return () => {
+      console.log("Componente Destruído!");
+    };
+  }, []);
+
+  // Eventos
+  function addTodo(event) {
+    event.preventDefault();
+    const novoTodo = inputText.current.value;
+    const novosValoresDoTodo = [...todos, novoTodo];
+    setTodos(novosValoresDoTodo);
+    localStorage.setItem("todos", JSON.stringify(novosValoresDoTodo));
+    inputText.current.value = "";
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" placeholder="Escreve o seu TODO" ref={inputText} />
+        <input type="submit" value="Novo TODO" />
+      </form>
+    </div>
+  );
+};
+
+export default Todo;
